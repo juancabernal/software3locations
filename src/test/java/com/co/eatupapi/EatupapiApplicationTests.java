502c86aaf9264a05c9c8ce256b8a65b95d55be3a
juannobl <jpnorena@sura.com.co>
2026-03-07 10:15:47 -0500

    se inicializa poryecto

---
+++ src/test/java/com/co/eatupapi/EatupapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.eatupapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatupapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

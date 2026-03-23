1cbae71856eae4b454c07eb0a106210e403b1dac
Alejo Arbelaez <alejoarbelaez@MacBook-Air-de-Alejo.local>
2026-03-23 14:20:32 -0500

    Resolve merge conflicts with develop

--- src/main/java/com/co/eatupapi/domain/inventory/transfer/Transfer.java
+++ src/main/java/com/co/eatupapi/domain/inventory/transfer/Transfer.java
@@ -1,7 +1,7 @@
 package com.co.eatupapi.domain.inventory.transfer;
 
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
+import java.time.LocalDateTime; 
 
 @Entity
 @Table(name = "transfers")

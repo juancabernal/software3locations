9e4b1c50af3817dac80812d0f927385895883dc8
juan jose <juanjosenarvaezmarin13092005@gmail.com>
2026-03-28 10:02:37 -0500

    renombrando handler

--- src/main/java/com/descuentos/descuentos_mio/utils/customerdiscount/exceptions/CustomerDiscountGlobalExceptionHandler.java
+++ src/main/java/com/descuentos/descuentos_mio/utils/customerdiscount/exceptions/CustomerDiscountGlobalExceptionHandler.java
@@ -1,14 +1,14 @@
-package com.descuentos.descuentos_mio.utils.discount.exceptions;
+package com.descuentos.descuentos_mio.utils.customerdiscount.exceptions;
 
-import com.descuentos.descuentos_mio.controller.DiscountController;
+import com.descuentos.descuentos_mio.controller.CustomerDiscountController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
 
-@RestControllerAdvice(assignableTypes = DiscountController.class)
-public class GlobalExceptionHandler {
+@RestControllerAdvice(assignableTypes = CustomerDiscountController.class)
+public class CustomerDiscountGlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Map<String, String>> handleValidationErrors(IllegalArgumentException ex) {
--- src/main/java/com/descuentos/descuentos_mio/utils/discount/exceptions/DiscountGlobalExceptionHandler.java
+++ src/main/java/com/descuentos/descuentos_mio/utils/discount/exceptions/DiscountGlobalExceptionHandler.java
@@ -1,14 +1,14 @@
-package com.descuentos.descuentos_mio.utils.customerdiscount.exceptions;
+package com.descuentos.descuentos_mio.utils.discount.exceptions;
 
-import com.descuentos.descuentos_mio.controller.CustomerDiscountController;
+import com.descuentos.descuentos_mio.controller.DiscountController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
 
-@RestControllerAdvice(assignableTypes = CustomerDiscountController.class)
-public class GlobalExceptionHandler {
+@RestControllerAdvice(assignableTypes = DiscountController.class)
+public class DiscountGlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Map<String, String>> handleValidationErrors(IllegalArgumentException ex) {

656906dbc12f121a752f982195a49bb238bdba1c
Maicol Oviedo <165244378+Maicol-Oviedo@users.noreply.github.com>
2026-04-09 23:30:18 -0500

    Se actualizan los mensajes de error en las pruebas de recetas. Se modifica la anotación de RecipeExceptionHandler para reflejar la nueva estructura de paquetes y se establece un orden de precedencia.

--- src/main/java/com/co/eatupapi/utils/inventory/recipe/exceptions/RecipeExceptionHandler.java
+++ src/main/java/com/co/eatupapi/utils/inventory/recipe/exceptions/RecipeExceptionHandler.java
@@ -1,12 +1,15 @@
 package com.co.eatupapi.utils.inventory.recipe.exceptions;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(basePackages = "com.co.eatupapi.services.inventory.recipe")
+@RestControllerAdvice(basePackages = "com.co.eatupapi.controllers.inventory.recipe")
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class RecipeExceptionHandler {
 
     @ExceptionHandler(RecipeNotFoundException.class)
